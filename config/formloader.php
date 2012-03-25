bfe7456fcf2ce95c083e8699a14bbf47c155b1fc
Tim Griesser <tgriesser10@gmail.com>
2012-03-25 13:07:39 -0400

    Fixing some module path things

--- config/formloader.php
+++ config/formloader.php
@@ -119,7 +119,7 @@ return array(
 				 * Adds the module path and module
 				 */
 				$module_paths = Config::get('module_paths');
-				array_push($module_paths, __DIR__ . '/../modules/');
+				array_unshift($module_paths, __DIR__ . '/../modules/');
 				Config::set('module_paths', $module_paths);
 				Fuel::add_module('formloader');
 
