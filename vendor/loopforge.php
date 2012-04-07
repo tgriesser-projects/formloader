7cef00be7ac829c61510c8eeff1af5053d8e19fb
Tim Griesser <tgriesser10@gmail.com>
2012-04-07 16:47:23 -0400

    moved array_key_collapse to loopforge class

--- vendor/loopforge.php
+++ vendor/loopforge.php
@@ -102,6 +102,38 @@ abstract class Loopforge
 			return array_filter($array);
 		}
 	}
+
+	/**
+	 * Checks if the array's keys/sub-keys aren't (int)...
+	 * if so it adds them to a stack of collapsed keys
+	 * @param array
+	 */
+	public static function array_key_collapse($array)
+	{
+		$stack = array();
+		$depth = array();
+		$collapse = function($val, $key, $func) use(&$stack, &$depth)
+		{
+			array_push($depth, $key);
+			if (is_array($val))
+			{
+				$stack[] = implode('.', $depth);
+				array_walk($val, $func, $func);
+			}
+			else
+			{
+				if ( ! is_int($key))
+				{
+					$stack[] = implode('.', $depth);
+				}
+			}
+			array_pop($depth);
+		};
+		
+		array_walk($array, $collapse, $collapse);
+		
+		return $stack;
+	}
 	
 	/**
 	 * Removes the prefixed keys from the array
